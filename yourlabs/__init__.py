52807e9f7757c3e55506928cfcb8decc52e1ab73
KDUser <128939316+KDUser12@users.noreply.github.com>
2024-08-04 10:45:39 +0200

    Versioning
    - Version management of required modules
    - Ignore file extension `.pyc`.
    - Management of the program version.

---
+++ yourlabs/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A program to facilitate developers on the management of their personal
+projects in python.
+"""
+
+import _version
+
+
+__version__ = _version.__version__
+__author__ = "KDUser12"
---
+++ yourlabs/__main__.py
@@ -0,0 +1,6 @@
+from __init__ import __version__, __author__
+from _version import release_date
+
+
+if __name__ == '__main__':
+    print(__version__, __author__, release_date)    
