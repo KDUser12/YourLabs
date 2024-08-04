14219df52dc3208fbfb51df3a6ba500da915b039
KDUser <128939316+KDUser12@users.noreply.github.com>
2024-08-04 17:19:20 +0200

    Debug mode management
    - Removing unnecessary imports
    - Implementing debug mode

--- yourlabs/__main__.py
+++ yourlabs/__main__.py
@@ -14,25 +14,31 @@ Released under the MIT License
 """
 
 import logging
-import os
+import argparse
 
 from utils._os import check_os_compatibility
 from utils.env import check_python_version
 from utils.packages import check_and_install_dependencies
 
-# Configure logging
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
 
-logging.basicConfig(
-    level=LOG_LEVEL,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler('logs.log', mode='a')
-    ]
-)
+def setup_logging(debug=False):
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler('logs.log', mode='a')
+        ]
+    )
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="A program to facilitate developers on the management of their personal projects in python.")
+    parser.add_argument('--debug', action='store_true', help='enable debug mode')
+    
+    args = parser.parse_args()
+    
+    setup_logging(args.debug)
+    
     required_versions = ['3.6', '3.7', '3.8']
     logging.debug(f"Required versions : {required_versions}")
     
